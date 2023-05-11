2bdb51f1cca9e445dde13dde989f23d42412c859
Luis Mendoza <luis.mendoza.alonso@hotmail.com>
2023-05-11 16:55:45 -0500

    Material y Rutas
    Se realiza instalacion de materia y configuracion de rutas

---
+++ src/app/modules/material/material.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import {MatSliderModule} from '@angular/material/slider';
+
+
+
+@NgModule({
+  declarations: [],
+  
+  exports:[
+    MatSliderModule
+  ]
+})
+export class MaterialModule { }
