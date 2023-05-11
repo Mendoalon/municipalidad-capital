70883d84ef50721810df5814fc6b5d0328a3b461
Luis Mendoza <luis.mendoza.alonso@hotmail.com>
2023-05-11 17:16:25 -0500

    Sidebar y Navbar
    Se realiza creacion de Sidebar y Navbar

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SidebarComponent,
+    NavbarComponent
+  ],
   imports: [
     CommonModule
   ]
